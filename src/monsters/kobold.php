ec1a7d912f5480c700de9bb1d1d0d1ed46d38e87
Coding-Diligence <kilian.poussard.pro@gmail.com>
2024-09-26 11:24:52 +0200

    Ajout monster

---
+++ src/monsters/kobold.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Jugid\Staurie\Example\Monsters;
+
+use Jugid\Staurie\Game\Monster;
+
+class Kobold extends Monster {
+
+    public function name() : string {
+        return 'Kobold';
+    }
+
+    public function description(): string { 
+        return 'This monster look like a dog but is as powerful as an adult human';
+    }
+
+    public function level() : int {
+        return 2;
+    }
+
+    public function health_points(): int { 
+        return 20;
+    }
+
+    public function defense(): int { 
+        return 3;
+    }
+
+    public function experience(): int { 
+        return 30;
+    }
+
+    public function skills(): array { 
+        return [
+            'Claw Strike' => 15,
+        ];
+    }
+}
