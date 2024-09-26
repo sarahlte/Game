f6015a09190d69fd142634d2c01d1a933379e429
sarahlte <sarah_leconte@msn.com>
2024-09-26 11:37:48 +0200

    Update main.php

--- main.php
+++ main.php
@@ -2,7 +2,9 @@
 
 use Jugid\Staurie\Component\Console\Console;
 use Jugid\Staurie\Component\Menu\Menu;
+use Jugid\Staurie\Component\Map\Map;
 use Jugid\Staurie\Component\Introduction\Introduction;
+use Jugid\Staurie\Component\Money\Money;
 use Jugid\Staurie\Component\PrettyPrinter\PrettyPrinter;
 use Jugid\Staurie\Staurie;
 
@@ -24,6 +26,15 @@ $menu->configuration([
 
 echo chr(27).chr(91).'H'.chr(27).chr(91).'J'; 
 
+$map = $container->registerComponent(Map::class);
+$map->configuration([
+    'directory'=>__DIR__.'/maps',
+    'namespace'=>'src\Maps', 
+    'navigation'=>true,
+    'map_enable'=>true,
+    'compass_enable'=>true
+]);
+
 $introduction = $container->registerComponent(Introduction::class);
 $introduction->configuration([
     'text'=>[
@@ -32,6 +43,12 @@ $introduction->configuration([
     ],
     'title'=>'Chapter 1 : The enchanted forest.',
     'scrolling'=>false
+]);
+
+$money = $container->registerComponent(Money::class);
+$money->configuration([
+    'name' => 'Clochette',
+    'start_with' => 0
 ]);
 
 $staurie->run(); //LANCE LE JEU
