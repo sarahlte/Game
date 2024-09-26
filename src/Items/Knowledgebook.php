e257fd07a9fe4401d600d2e7522b111020780d7f
maelanglois <150166864+maelanglois@users.noreply.github.com>
2024-09-26 15:51:47 +0200

    Update Knowledgebook.php

--- src/Items/Knowledgebook.php
+++ src/Items/Knowledgebook.php
@@ -4,7 +4,7 @@ namespace Rpg\Game\Items;
 
 use Jugid\Staurie\Game\Item_Equippable;
 
-class Balloon extends Item_Equippable {
+class KnowledgeBook extends Item_Equippable {
 
     public function name() : string {
         return 'Book of knowledge';
