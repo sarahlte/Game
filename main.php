e467d024c0c7bd3b543da659d93768145dff96d5
maelanglois <150166864+maelanglois@users.noreply.github.com>
2024-09-26 10:39:44 +0200

    add
    description

--- main.php
+++ main.php
@@ -7,14 +7,14 @@ use Jugid\Staurie\Staurie;
 
 require_once __DIR__.'/vendor/autoload.php'; //A REMPLACER
 
-$staurie = new Staurie('My game');
+$staurie = new Staurie('Zodiac adventures');
 $staurie->register([Console::class, PrettyPrinter::class, Menu::class]);
 
 $container = $staurie->getContainer();
 
 $menu = $container->registerComponent(Menu::class);
 $menu->configuration([
-  'text'=> 'Welcome to this awesome test adventure',
+  'text'=> 'Welcome to this awesome adventure',
   'labels'=> [
   'new_game' => 'Enter the world',
   'quit'=> 'Exit game',
