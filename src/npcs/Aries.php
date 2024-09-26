da836fd9c189f8cf782c3c4ed152d46b9b3c4ee2
maelanglois <150166864+maelanglois@users.noreply.github.com>
2024-09-26 10:59:38 +0200

    Create Aries.php

---
+++ src/npcs/Aries.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Jugid\Staurie\Example\Npcs;
+
+use Jugid\Staurie\Example\Items\Shield;
+use Jugid\Staurie\Game\Npc;
+
+class Aries extends Npc {
+
+    public function name() : string {
+        return 'Aries';
+    }
+
+    public function description() : string {
+        return 'A man with his face hidden by his long hair… A mysterious aura emanates from him.';
+    }
+
+    public function speak() : string|array {
+        return 'Heus adolescentulo, quod te mihi fatum attulit';
+    }
+
+}
