2991bba5ca59f47d0c7b3a10332a550ddcfabc51
maelanglois <150166864+maelanglois@users.noreply.github.com>
2024-09-26 14:09:27 +0200

    Create Aries.php

---
+++ src/npcs/Aries.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Rpg\Game\Npcs;
+
+use Rpg\Game\Items\Potion;
+use Jugid\Staurie\Game\Npc;
+
+class Aries extends Npc {
+
+    public function name() : string {
+        return 'Aries';
+    }
+
+    public function description() : string {
+        return 'A man with his face hidden by his long white hair… Big horns are adorning his head.';
+    }
+
+    public function speak() : string|array {
+        $this->giveItem(new Potion());
+        return ['Heus adolescentulo, quod te mihi fatum attulit',
+        '(the man seems to speak a language you do not understand.) (he ends up giving you a shield and disappearing.)',
+        '(not understanding a thing, you choose to take the shield and continue on your adventure)'];
+    }
+
+}
