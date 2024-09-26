d08462dd5af8ea8c8882068068f6b1c9e4d612d2
Coding-Diligence <kilian.poussard.pro@gmail.com>
2024-09-26 11:55:58 +0200

    Fluffy

---
+++ src/monsters/Fluffy.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace src\Monsters;
+
+use Jugid\Staurie\Game\Monster;
+
+class Fluffy extends Monster {
+
+    public function name() : string {
+        return 'Fluffy';
+    }
+
+    public function description(): string { 
+        return 'The cutest thing alive';
+    }
+
+    public function level() : int {
+        return 50;
+    }
+
+    public function health_points(): int { 
+        return 5000;
+    }
+
+    public function defense(): int { 
+        return 100;
+    }
+
+    public function experience(): int { 
+        return 10000000;
+    }
+
+    public function skills(): array { 
+        return [
+            'Big Lick' => 100000000000000000,
+        ];
+    }
+}
