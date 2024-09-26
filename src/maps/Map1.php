bdcd145896d08fbffb58afdd83db6324b8ec60f9
sarahlte <sarah_leconte@msn.com>
2024-09-26 11:01:25 +0200

    Create Map1.php

---
+++ src/maps/Map1.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace src\Maps;
+
+use Jugid\Staurie\Component\Map\Blueprint;
+use Jugid\Staurie\Game\Position\Position;
+
+class Map1 extends Blueprint {
+  private Position $position;
+
+  public function __construct()
+  {
+      $this->position = new Position(0,0);
+  }
+
+  public function name(): string {
+    return 'Enchanted forest';
+  }
+
+  public function description(): string { 
+    return '';
+  }
+  
+  public function position(): Position {
+    return $this->position;
+  }
+
+  public function npcs(): array {
+    return [];
+  }
+
+  public function items(): array {
+    return [];
+  }
+
+  public function monsters(): array {
+    return [];
+  }
+
+}
