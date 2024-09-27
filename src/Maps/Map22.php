1f812dfbc863cf7661c4d3a57fe9aac662d2df11
maelanglois <150166864+maelanglois@users.noreply.github.com>
2024-09-27 15:27:14 +0200

    Update Map22.php

--- src/Maps/Map22.php
+++ src/Maps/Map22.php
@@ -4,6 +4,7 @@ namespace Rpg\Game\Maps;
 
 use Jugid\Staurie\Component\Map\Blueprint;
 use Jugid\Staurie\Game\Position\Position;
+use Rpg\Game\Monsters\IronSlime;
 
 class Map22 extends Blueprint {
   private Position $position;
@@ -34,7 +35,7 @@ class Map22 extends Blueprint {
   }
 
   public function monsters(): array {
-    return [];
+    return [new IronSlime()];
   }
 
 }
