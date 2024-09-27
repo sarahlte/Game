acea434d771c05744a36b9fb21e7ecf3a775b1c1
sarahlte <sarah_leconte@msn.com>
2024-09-27 16:12:58 +0200

    Update Aquarius.php

--- src/Npcs/Aquarius.php
+++ src/Npcs/Aquarius.php
@@ -6,7 +6,7 @@ use Rpg\Game\Items\WaterVessel;
 use Jugid\Staurie\Game\Npc;
 
 class Aquarius extends Npc {
-    private int $limitUse;
+    private int $limitUse = 0;
 
     public function name() : string {
         return 'Aquarius';
